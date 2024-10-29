82f8b0702473ed4aed1346ed46c1c686d3b122dd
Frederic Wibaux <fwibaux@gmail.com>
2024-10-29 12:27:15 +0100

    push oct 24

--- Gemfile
+++ Gemfile
@@ -1,12 +1,14 @@
+# If you do not have OpenSSL installed, update
+# the following line to use "http://" instead
 source 'https://rubygems.org'
 
-gem 'middleman', '~> 3.4'
+gem "middleman", "~>3.4.1"
 
-# Deploy to GitHub Pages
-gem 'middleman-deploy', '~> 1.0'
+# Live-reloading plugin
+gem "middleman-livereload", "~> 3.3.0"
 
-# Assets
-gem 'jquery-middleman'
-gem 'bootstrap-sass', '3.3.6'
-gem 'font-awesome-sass', '4.5.0'
-gem 'middleman-autoprefixer'
+# For faster file watcher updates on Windows:
+gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
+
+# Windows does not come with time zone data
+gem "tzinfo-data", platforms: [:mswin, :mingw, :jruby]
--- config.rb
+++ config.rb
@@ -7,9 +7,6 @@
 #   config.output_style = :compact
 # end
 
-# Auto-prefixing of CSS code with vendor prefix
-activate :autoprefixer
-
 ###
 # Page options, layouts, aliases and proxies
 ###
@@ -27,7 +24,7 @@ activate :autoprefixer
 #   page "/admin/*"
 # end
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
@@ -68,26 +65,8 @@ configure :build do
   # activate :asset_hash
 
   # Use relative URLs
-  activate :relative_assets
-  set :relative_links, true
+  # activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
-  #
-  require_relative "./lib/build_cleaner"
-  activate :build_cleaner
 end
-
-# Deployment
-activate :deploy do |deploy|
-  deploy.method = :git
-  deploy.build_before = true
-
-  # Optional Settings
-  # deploy.remote = 'custom-remote' # remote name or git url, default: origin
-  # deploy.branch = 'custom-branch' # default: gh-pages
-  # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
-  # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
-end
-
-activate :i18n
